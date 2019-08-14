184df6129d77ece4c97e369df37acab32f3eb903
Vinicius Pulgatti <viniciusFortti>
2019-08-14 13:56:18 -0300

    atualização.

--- src/main/java/spring/config/server/demo/SpringCloudConfigServerApplication.java
+++ src/main/java/spring/config/server/demo/SpringCloudConfigServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class DemoApplication {
+public class SpringCloudConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
     }
 
 }
